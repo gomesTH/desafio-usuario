48cb17808344fa496e0994f4de9c88747e2bba3a
Thiago Gomes <thiago.gpixel@gmail.com>
2021-05-31 11:31:44 -0300

    Novo Projeto

---
+++ usuario.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect('mongodb+srv://cadastro:cadastro@cluster0.qf8op.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+
+let UsuarioModel = mongoose.model
+  ('usuario', mongoose.Schema(
+    {
+      nome: {
+        type: String
+      },
+      nascimento: {
+        type: String
+      },
+      idade: {
+        type: Number
+      },
+      cpf: {
+        type: String
+      },
+      cep: {
+        type: String
+      },
+      endereco: {
+        type: String
+      },
+    }));
+
+
+module.exports = UsuarioModel;
+
+
